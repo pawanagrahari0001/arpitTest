683d1641f32d219ee870cf087b69097715087c3b
Techugo <bhavna@techugo.com>
2019-11-11 11:04:15 +0530

    Changes on Master

--- arpitTestDemo/AppDelegate.swift
+++ arpitTestDemo/AppDelegate.swift
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func oldMaster(){
         print("Old")
-        print("Develop new for conflict")
+        print("Develop Master for conflict")
     }
     
     func newF(){
