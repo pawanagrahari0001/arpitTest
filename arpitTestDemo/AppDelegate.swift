c69bb578bfee98774f64c9f311fadaee8e57e85d
Arpit Javerya <arpit@techugo.com>
2019-11-11 11:07:17 +0530

    arpit Commit

--- arpitTestDemo/AppDelegate.swift
+++ arpitTestDemo/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func oldMaster(){
+    func oldMasterOld(){
         print("Old")
         print("Develop Master for conflict")
     }
