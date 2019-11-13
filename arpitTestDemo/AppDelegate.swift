e55463cfb56ce19cb984bef0ea8f2c016ad7e453
Arpit Javerya <arpit@techugo.com>
2019-11-13 16:58:03 +0530

    Arpit chnages

--- arpitTestDemo/AppDelegate.swift
+++ arpitTestDemo/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print("fasdfasd")
     }
     
-    func newFfadsfTest(){
+    func newFfadsfTestadfasdfasd(){
         print("Newfdasf")
         print("fasdfaj")
     }
